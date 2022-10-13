9fb28e41637f8dc398c39e235c26d7bc73634b7b
Hubert Jozwiak <hjozwiakdx@gmail.com>
2022-10-13 22:34:15 +0200

    extended

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:hand_signature/signature.dart';
+import 'package:scribble/scribble.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,75 +20,155 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String title = 'Title ///';
 
+  ScrollController _scrollController = ScrollController();
+  TextDirection _textDirection = TextDirection.ltr;
+
+  double x = 0;
+  double y = 0;
+  double z = 0;
+
+  Matrix4 _matrix4 = Matrix4.identity()..setEntry(3, 2, 0.005);
+
+  double _scroll = -100.0;
+
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-      theme: const CupertinoThemeData(
-        barBackgroundColor: CupertinoColors.systemGroupedBackground,
+    return Theme(
+      data: ThemeData(
+        primarySwatch: Colors.blue,
+        // splashColor: Colors.red,
+        // highlightColor: Colors.red,
       ),
-      home: Directionality(
-        textDirection: TextDirection.ltr,
-        child: CupertinoPageScaffold(
-          child: CustomScrollView(
-            slivers: [
-              CupertinoSliverNavigationBar(
-                largeTitle: Text(title),
-                stretch: true,
-                border: null,
-              ),
-              SliverToBoxAdapter(
-                child: ColoredBox(
-                  color: CupertinoColors.systemGroupedBackground,
-                  child: CupertinoListSection.insetGrouped(
-                    children: [
-                      CupertinoTextFormFieldRow(
-                        onChanged: (value) => setState(() {
-                          title = value;
-                        }),
-                        placeholder: 'title here',
-                      ),
-                    ],
+      child: CupertinoApp(
+        theme: const CupertinoThemeData(
+          barBackgroundColor: CupertinoColors.systemGroupedBackground,
+        ),
+        home: Directionality(
+          textDirection: _textDirection,
+          child: CupertinoPageScaffold(
+            child: CustomScrollView(
+              controller: _scrollController,
+              slivers: [
+                CupertinoSliverNavigationBar(
+                  largeTitle: FilledButton(
+                    style: ButtonStyle(
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      shape:
+                          MaterialStatePropertyAll(ContinuousRectangleBorder()),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 70.0),
+                      child: Text('hit test'),
+                    ),
+                    onPressed: () {},
+                  ),
+                  // largeTitle: Row(),
+                  // largeTitle: Text(title),
+                  // middle: Text(title),
+                  stretch: true,
+                  border: null,
+                ),
+                SliverToBoxAdapter(
+                  child: ColoredBox(
+                    color: CupertinoColors.systemGroupedBackground,
+                    child: CupertinoListSection.insetGrouped(
+                      children: [
+                        CupertinoTextFormFieldRow(
+                          onChanged: (value) => setState(() {
+                            title = value;
+                          }),
+                          placeholder: 'title here',
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            FilledButton(
+                              child: const Text('scroll'),
+                              onPressed: () async {
+                                await _scrollController.animateTo(
+                                  _scroll,
+                                  duration: const Duration(milliseconds: 1000),
+                                  curve: Curves.easeInOut,
+                                );
+
+                                _scrollController.position.hold(() {});
+                              },
+                            ),
+                            Card(
+                              child: Row(
+                                children: [
+                                  Slider(
+                                      min: -500,
+                                      max: 0,
+                                      value: _scroll,
+                                      onChanged: (newScrollTarget) {
+                                        setState(() {
+                                          _scroll = newScrollTarget;
+                                        });
+                                      }),
+                                  Text(_scroll.toInt().toString()),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                        FilledButton(
+                          child: const Text('back'),
+                          onPressed: () {
+                            _scrollController.position.hold(() {}).cancel();
+                          },
+                        ),
+                        FilledButton(
+                          child: const Text('switch LTR/RTL'),
+                          onPressed: () {
+                            setState(() {
+                              _textDirection =
+                                  _textDirection == TextDirection.ltr
+                                      ? TextDirection.rtl
+                                      : TextDirection.ltr;
+                            });
+                          },
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Transform(
+                            transform: _matrix4,
+                            alignment: FractionalOffset.center,
+                            child: GestureDetector(
+                              onPanUpdate: (details) {
+                                setState(() {
+                                  _matrix4
+                                    ..rotateX(-details.delta.dy / 500)
+                                    ..rotateY(details.delta.dx / 500);
+                                });
+                              },
+                              child: SizedBox(
+                                height: 200,
+                                width: 200,
+                                child: Material(
+                                  color: Colors.red,
+                                  child: InkWell(
+                                    onTap: () {},
+                                    child: const FlutterLogo(),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              const SliverFillRemaining(
-                child: ColoredBox(
-                  color: CupertinoColors.systemGroupedBackground,
-                ),
-              ),
-            ],
+                // const SliverFillRemaining(
+                //   child: ColoredBox(
+                //     color: CupertinoColors.systemGroupedBackground,
+                //   ),
+                // ),
+              ],
+            ),
           ),
         ),
-      ),
-    );
-  }
-}
-
-class NextPage extends StatelessWidget {
-  const NextPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      child: CustomScrollView(
-        slivers: [
-          const CupertinoSliverNavigationBar(
-            largeTitle: Text("Next page"),
-            stretch: true,
-            border: null,
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: CupertinoButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: const Text("Go back"),
-              ),
-            ),
-          )
-        ],
       ),
     );
   }
